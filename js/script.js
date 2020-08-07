fbfc11fee67475d5af109a099e12734cd864ae17
kotlyarDG <kotlyar.d.g@gmail.com>
2020-08-07 18:31:21 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 // Проверка, загрузилась ли страница
-$(document).ready(function(){
+$(document).ready(function () {
+	$('.header__burger').click(function (event) {
+		$('.header__burger,.header__nav').toggleClass('_active')
+		// $('body').toggleClass('_lock')
+	});
 
+	$('.slider-about').slick({
+		dots: true,
+		slidesToShow: 3,
+		centerMode: true,
+		variableWidth: true,
+		responsive: [
+			{
+				breakpoint: 991,
+				settings: {
+					centerMode: true,
+					slidesToShow: 1,
+					arrows: false
+				}
+			}
+		]
+
+	});
 });
