01ad9e7ab54b2299094d281bad753d70a5c0fb3a
kotlyarDG <kotlyar.d.g@gmail.com>
2020-08-08 12:37:06 +0300

    slider-info

--- js/script.js
+++ js/script.js
@@ -22,4 +22,40 @@ $(document).ready(function () {
 		]
 
 	});
+
+	$('.slider-logo').slick({
+		dots: false,
+		slidesToShow: 5,
+		infinite: true,
+		centerMode: true,
+		speed: 800,
+		asNavFor: '.slider-info',
+		responsive: [
+			{
+				breakpoint: 991,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					arrows: false
+				}
+			}
+		]
+
+	});
+
+	$('.slider-info').slick({
+		dots: false,
+		arrows: false,
+		slidesToShow: 1,
+		speed: 800,
+		infinite: true,
+		asNavFor: '.slider-logo'
+
+
+	});
 });
